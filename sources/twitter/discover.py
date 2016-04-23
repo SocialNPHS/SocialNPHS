809b973a3815d053b9fab6010001bd26ad4177b3
Pråppe <pw.td.gk@gmail.com>
2016-04-23 18:02:59 -0400

    starting on basic twitter implementation

---
+++ sources/twitter/discover.py
@@ -0,0 +1,8 @@
+"""
+Finds possible Twitter accounts of NPHS students by finding other users
+that follow or are followed by confirmed NPHS accounts and are in or near
+the New Paltz area.
+"""
+
+import tweepy
+import user
---
+++ sources/twitter/user.py
@@ -0,0 +1,33 @@
+"""
+Base class for a twitter user
+"""
+
+import json
+import tweepy
+
+# Retrieves Twitter API tokens & keys
+with open('token.json') as f:
+    t = json.load(f)
+    CONSUMER_KEY = t['CONSUMER_KEY']
+    CONSUMER_SECRET = t['CONSUMER_SECRET']
+    ACCESS_TOKEN = t['ACCESS_TOKEN']
+    ACCESS_SECRET = t['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+api = tweepy.API(auth)
+
+
+class NPUser(object):
+    def __init__(self, screen_name):
+        self.screen_name = screen_name
+        self.User = api.get_user(self.screen_name)
+
+    @property
+    def following():
+        """ List of users that this user follows """
+        return self.User.friends()
+
+    @property
+    def followers():
+        """ List of users following this user """
+        return self.User.followers()
