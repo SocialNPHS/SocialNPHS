f2b175c5128c5fbf4416dc9cf847095b86ee7693
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-04-23 22:31:21 -0400

    Clean up code
    It's best practice for scripts not run directly to limit the amount of code that just sits there in the beginning of the script

--- sources/twitter/user.py
+++ sources/twitter/user.py
@@ -6,28 +6,47 @@ import json
 
 import tweepy
 
+# -------- HELPER METHODS -------- #
 
-# Retrieves Twitter API tokens & keys from cached JSON store
-with open('token.json') as f:
-    t = json.load(f)
-    CONSUMER_KEY = t['CONSUMER_KEY']
-    CONSUMER_SECRET = t['CONSUMER_SECRET']
-    ACCESS_TOKEN = t['ACCESS_TOKEN']
-    ACCESS_SECRET = t['ACCESS_SECRET']
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-api = tweepy.API(auth)
+def _get_secret_stuffs():
+    """ Retrieves Twitter API tokens & keys from stored secrets """
+    secrets = ["CONSUMER_KEY",
+               "CONSUMER_SECRET",
+               "ACCESS_TOKEN",
+               "ACCESS_SECRET"]
+    with open("token.json") as f:
+        t = json.load(f)
+        return [t[s] for s in secrets]
 
-with open('users.json') as f:
-    Students = json.load(f)
+
+def _get_api():
+    """ Gets an authenticated tweepy API instance from stored JSON secrets """
+    secrets = _get_secret_stuffs()
+    auth = tweepy.OAuthHandler(*secrets[:2])
+    auth.set_access_token(*secrets[2:])
+    return tweepy.API(auth)
+
+
+def get_users_dict():
+    with open('users.json') as f:
+        return json.load(f)
+
+
+# -------- GLOBALS -------- #
+
+API = _get_api()
+students = get_users_dict()
+
+
+# -------- MAIN CLASSES -------- #
 
 
 class NPUser(object):
     def __init__(self, screen_name):
         self.screen_name = screen_name
-        self.user_info = Students[self.screen_name]
-        self.User = api.get_user(self.screen_name)
+        self.user_info = students[self.screen_name]
+        self.User = API.get_user(self.screen_name)
 
     @property
     def following(self):
@@ -42,7 +61,7 @@ class NPUser(object):
     @property
     def grade(self):
         """ Returns colloquial grade name of user """
-        grade = Students[self.screen_name]['grade']
+        grade = self.user_info['grade']
         return {
             '2016': 'Senior', '2017': 'Junior',
             '2018': 'Sophomore', '2019': 'Freshman'
@@ -56,6 +75,9 @@ class NPUser(object):
                                  "instance has no attribute '" + key + "'")
         else:
             return self.user_info[key]
+
+
+# -------- TESTS -------- #
 
 if __name__ == "__main__":
     a = NPUser("1Defenestrator")
