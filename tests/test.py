1c12e698f8cb794002e4c1832e6fec31484359c6
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-05-23 20:30:18 -0400

    Glob is actually magical

--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,11 @@
 """ Run all tests. """
+import glob
 import os
 
 localdir = os.path.dirname(os.path.abspath(__file__))
 
 
-def listTests(folder):
-    """ List all the tests in a folder of tests (files that end with .py) """
-    folder = os.path.abspath(folder)
-    return [os.path.join(folder, i) for i in os.listdir(folder)
-            if i.endswith(".py")]
-
-# All files in the same directory as this file
-subfiles = [os.path.join(localdir, fi) for fi in os.listdir(localdir)]
-# All directories inside the directory this file is in
-testDirs = [d for d in subfiles if os.path.isdir(d)]
-# Find tests in identified test directories, and flatten
-tests = sum([listTests(td) for td in testDirs], [])
+tests = glob.glob(os.path.join(localdir, "*/*.py"))
 
 for test in tests:
     with open(test) as f:
