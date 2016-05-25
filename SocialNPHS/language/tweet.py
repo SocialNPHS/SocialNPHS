b7978d9fa40891c82369a5d709484d142027d311
Pråppe <pw.td.gk@gmail.com>
2016-05-24 21:35:28 -0400

    Basic tweet sentiment analysis

--- SocialNPHS/language/tweet.py
+++ SocialNPHS/language/tweet.py
@@ -3,7 +3,9 @@ Given a tweet, tokenize it and shit.
 """
 
 import nltk
-from nltk.tokenize import TweetTokenizer
+from nltk.tag import util
+from nltk.tokenize import sent_tokenize, TweetTokenizer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 from SocialNPHS.sources.twitter.auth import api
 from SocialNPHS.sources.twitter import user
@@ -28,3 +30,46 @@ def get_tweet_tags(tweet):
                 usr = api.get_user(handle)
                 tokens[n] = usr.name
     return nltk.pos_tag(tokens)
+
+
+def tweet_connotation(tweet):
+    """ Decide whether a tweet is generally positive or negative """
+    anlyzr = SentimentIntensityAnalyzer()
+    # break tweet up into sentences and analyze each seperately
+    twtcontent = sent_tokenize(tweet)
+    overall = {'compound': 0, 'neg': 0, 'neu': 0, 'pos': 0}
+    for s in twtcontent:
+        scores = anlyzr.polarity_scores(s)
+        # tally up each sentence's overall tone
+        for i, z in enumerate(scores):
+            overall[z] += scores[z]
+    # average it all together for the tweet as a whole
+    for v in overall:
+        overall[v] = round(overall[v]/len(twtcontent), 3)
+    return overall
+
+
+def person_connotation(tweet, name):
+    """
+    Decide whether a person is talked favorably about or not, based on the
+    tone of the sentences in which their name appears
+    """
+    twtcontent = sent_tokenize(tweet)
+    overall = {'compound': 0, 'neg': 0, 'neu': 0, 'pos': 0}
+    mentions = 0
+    # analyze each sentence talking about `name` person
+    for s in twtcontent:
+        tags = get_tweet_tags(s)
+        # if the name appears in the tagged sentence, get its tone
+        if (name, 'NNP') in tags:
+            sentence = util.untag(tags)
+            scores = tweet_connotation(' '.join(sentence))
+            # add it up to the overall tweet's tone
+            for i, z in enumerate(scores):
+                overall[z] += scores[z]
+            mentions += 1
+    # averaging all sentences' scores. don't wanna divide by zero now do we
+    if mentions != 0:
+        for v in overall:
+            overall[v] = round(overall[v]/mentions, 3)
+    return overall
--- tests/language.py
+++ tests/language.py
@@ -24,6 +24,11 @@ class testLanguage(unittest.TestCase):
         tags = tweet.get_tweet_tags(moshe.tweepy.status.text)
         self.assertIsInstance(tags, list)
         # Test that names are replaced and recognized as proper nouns
-        tags = tweet.get_tweet_tags("@1Defenestrator is really awesome!")
+        x = "@1Defenestrator is really awesome! Although, @G4_Y5_3X is cooler."
+        tags = tweet.get_tweet_tags(x)
         self.assertEqual(tags[0][0], "Luke Taylor")
         self.assertEqual(tags[0][1], "NNP")
+        # Test that basic sentiment analysis works (current values hardcoded)
+        self.assertEqual(tweet.tweet_connotation(x)['compound'], 0.345)
+        self.assertEqual(tweet.person_connotation(x, 'Luke Taylor')['neu'],
+                         0.461)
