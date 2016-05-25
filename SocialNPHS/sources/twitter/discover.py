65e1a870e16544b2f2d840d3a9ce5c76912222a0
pw.td.gk <pw.td.gk@gmail.com>
2016-05-25 10:14:32 -0400

    Added more methods to discovery

--- SocialNPHS/sources/twitter/discover.py
+++ SocialNPHS/sources/twitter/discover.py
@@ -10,7 +10,7 @@ from SocialNPHS.sources.twitter.auth import api
 from SocialNPHS.sources.twitter import *
 
 
-def basic_discover(origin, num_users=20):
+def discover_by_location(origin, num_users=20):
     """
     Discovers users based on profile location, not post geolocation.
     `num_users` defaults to 20 because I'm pretty sure 20 is when the ratelimit
@@ -24,7 +24,7 @@ def basic_discover(origin, num_users=20):
     return np_users
 
 
-def discover(origin, num_users=20):
+def discover_by_geolocation(origin, num_users=20):
     """
     Discovers users based on the location posted with some of their tweets.
     """
@@ -52,4 +52,33 @@ def discover(origin, num_users=20):
                             continue  # coord is outside of np area :'c
                         np_users.append(u.screen_name)
                         break
+    return np_users
+
+def discover_by_association(origin, num_users=20, num_np_followers=5):
+    """
+    Discovers users based on the number of `NPUser`s that follow them.
+    """
+    users = random.sample(origin.following, num_users)
+    np_users = []
+    for u in users:
+        # go through this user's followers' followers to see how popular
+        # they are with np students
+        followers = u.followers
+        if len(followers) > 2000:
+            # nobody from new paltz would be super famous so we can skip them
+            continue
+        found_np_followers = 0
+        for p in u.followers:
+            if found_np_followers >= num_np_followers:
+                # if `u` has enough np followers you win a new a car
+                np_users.append(u.screen_name)
+                break
+            try:
+                # check if this follower is an npuser
+                _p = user.NPUser(p.screen_name)
+            except ValueError:
+                # well they aren't in the db so try the next dude
+                continue
+            else:
+                found_np_followers += 1
     return np_users
