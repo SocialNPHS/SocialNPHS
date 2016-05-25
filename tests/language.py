a778f69e0181ad2ac4b89d23d7cdfa30299e28c3
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-05-25 18:37:20 -0400

    Make the test sentence used in language tests an instance attribute
    Use cool implicit string joining

--- tests/language.py
+++ tests/language.py
@@ -18,7 +18,9 @@ from SocialNPHS.sources.twitter import user
 
 
 class testLanguage(unittest.TestCase):
-    words = "@1Defenestrator is really awesome! Although, @G4_Y5_3X is cooler."
+    def setUp(self):
+        self.sentence = ("@1Defenestrator is really awesome! Although, "
+                         "@G4_Y5_3X is cooler.")
 
     def test_tweet_tagging(self):
         # Test that tagging works in its most basic form
@@ -26,17 +28,17 @@ class testLanguage(unittest.TestCase):
         tags = tweet.get_tweet_tags(moshe.tweepy.status.text)
         self.assertIsInstance(tags, list)
         # Test that names are replaced and recognized as proper nouns
-        tags = tweet.get_tweet_tags(self.words)
+        tags = tweet.get_tweet_tags(self.sentence)
         self.assertEqual(tags[0][0], "Luke Taylor")
         self.assertEqual(tags[0][1], "NNP")
 
     def test_tweet_analysis(self):
         # Test that basic sentiment analysis works
         self.assertEqual(
-            tweet.tweet_connotation(self.words)['compound'],
+            tweet.tweet_connotation(self.sentence)['compound'],
             0.345
         )
         self.assertEqual(
-            tweet.person_connotation(self.words, 'Luke Taylor')['neu'],
+            tweet.person_connotation(self.sentence, 'Luke Taylor')['neu'],
             0.461
         )
