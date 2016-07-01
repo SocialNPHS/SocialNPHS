65a5a70586759abbe612c17b8134f7a162a3a998
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-07-01 12:29:42 -0400

    Rearrange tests to report accurately

--- tests/__main__.py
+++ tests/__main__.py
@@ -2,8 +2,8 @@
 Run all tests
 """
 import glob
-import unittest
 from os import path
+import unittest
 
 import coverage
 
@@ -16,11 +16,11 @@ tests = set(tests) - set(["__main__.py", "__init__.py"])
 # Names of modules to be imported
 testNames = [path.splitext(path.split(t)[1])[0] for t in tests]
 
-suites = [unittest.defaultTestLoader.loadTestsFromName(t) for t in testNames]
-fullsuite = unittest.TestSuite(suites)
-
 cov = coverage.Coverage(source=["SocialNPHS"])
 cov.start()
+
+suites = [unittest.defaultTestLoader.loadTestsFromName(t) for t in testNames]
+fullsuite = unittest.TestSuite(suites)
 
 unittest.TextTestRunner().run(fullsuite)
 
