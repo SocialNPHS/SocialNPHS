e11fadb6d9d3860dc9bcc2218bf8e25124ed1c13
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-06-30 21:17:11 -0400

    Fix typo in setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email="luke@deentaylor.com",
     url="http://luke.deentaylor.com/",
     install_requires=[
-        "livejson",
+        "nltk",
         "tweepy",
         "shapely",
         "livejson"
