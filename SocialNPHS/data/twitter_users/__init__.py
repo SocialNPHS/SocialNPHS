a3ecfe8e0a8e7fe0d210daa4b7d09b96a0fb22df
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-07-01 17:51:35 -0400

    Prettify database using livejson 1.8 features

--- SocialNPHS/data/twitter_users/__init__.py
+++ SocialNPHS/data/twitter_users/__init__.py
@@ -4,4 +4,4 @@ import livejson
 
 localdir = os.path.dirname(os.path.abspath(__file__))
 dbpath = os.path.join(localdir, "users.json")
-students = livejson.Database(dbpath)
+students = livejson.File(dbpath, pretty=True, sort_keys=True)
