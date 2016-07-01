48013da394e1b9c9bca5aa7a2c5c79764c7724ca
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-07-01 14:29:34 -0400

    Save coverage as artifact

--- tests/__main__.py
+++ tests/__main__.py
@@ -2,20 +2,20 @@
 Run all tests
 """
 import glob
-from os import path
+import os
 import sys
 import unittest
 
 import coverage
 
-localdir = path.dirname(path.abspath(__file__))
+localdir = os.path.dirname(os.path.abspath(__file__))
 
 # Absolute path to each testing script
-tests = glob.glob(path.join(localdir, "*.py"))
+tests = glob.glob(os.path.join(localdir, "*.py"))
 tests = set(tests) - set(["__main__.py", "__init__.py"])
 
 # Names of modules to be imported
-testNames = [path.splitext(path.split(t)[1])[0] for t in tests]
+testNames = [os.path.splitext(os.path.split(t)[1])[0] for t in tests]
 
 cov = coverage.Coverage(source=["SocialNPHS"])
 cov.start()
@@ -27,5 +27,13 @@ unittest.TextTestRunner().run(fullsuite)
 
 cov.stop()
 if "--report-coverage" in sys.argv:
-    print("\n")
-    cov.report(show_missing=True)
+    # Report coverage to either console or a build artifact, depending on
+    # whether we're inside CircleCI
+    inside_circle = "CIRCLE_ARTIFACTS" in os.environ
+    # We are inside CircleCI
+    if inside_circle:
+        export = os.path.join(os.environ["CIRCLE_ARTIFACTS"], "coverage.txt")
+        with open(export, "w") as f:
+            cov.report(show_missing=True, file=f)
+    else:
+        cov.report(show_missing=True)
