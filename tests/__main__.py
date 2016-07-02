c587b9c4954aceea23fe05891f6975a7096cc39e
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-07-01 21:34:13 -0400

    Add XML tests for Circle (hopefully)

--- tests/__main__.py
+++ tests/__main__.py
@@ -8,8 +8,14 @@ import sys
 import unittest
 
 import coverage
+import xmlrunner
 
+# GLOBALS ---------------------------------------------------------------------
 localdir = os.path.dirname(os.path.abspath(__file__))
+inside_circle = "CIRCLECI" in os.environ
+
+
+# SETUP -----------------------------------------------------------------------
 
 # On personal computers, .gitignored JSON files can be more convenient than
 # environment variables. If the "token.json" file is present, we copy its
@@ -21,6 +27,8 @@ if os.path.exists(tokenpath):
         os.environ.update(data)
 
 
+# ASSEMBLE TESTS --------------------------------------------------------------
+
 # Absolute path to each testing script
 tests = glob.glob(os.path.join(localdir, "*.py"))
 tests = set(tests) - set(["__main__.py", "__init__.py"])
@@ -28,22 +36,32 @@ tests = set(tests) - set(["__main__.py", "__init__.py"])
 # Names of modules to be imported
 testNames = [os.path.splitext(os.path.split(t)[1])[0] for t in tests]
 
+
+# RUN TESTS + REPORT ----------------------------------------------------------
+
+# Start coverage reporter
 cov = coverage.Coverage(source=["SocialNPHS"])
 cov.start()
 
 suites = [unittest.defaultTestLoader.loadTestsFromName(t) for t in testNames]
 fullsuite = unittest.TestSuite(suites)
 
-unittest.TextTestRunner().run(fullsuite)
-
-cov.stop()
-if "--report-coverage" in sys.argv:
-    # Report coverage to either console or a build artifact, depending on
-    # whether we're inside CircleCI
-    inside_circle = "CIRCLE_ARTIFACTS" in os.environ
-    # We are inside CircleCI
-    if inside_circle:
-        out = os.path.join(os.environ["CIRCLE_ARTIFACTS"], "coverage-report")
-        cov.html_report(directory=out)
-    print("\n")
-    cov.report(show_missing=True)
+if inside_circle:
+    # We're inside CircleCI. In this case, we output a rich HTML coverage
+    # report to the build artifacts section, and we provide JUnit-style XML
+    # reports for Circle to read.
+    xmlrunner.XMLTestRunner(
+        output=os.environ["CIRCLE_TEST_REPORTS"]
+    ).run(fullsuite)
+    cov.stop()
+    out = os.path.join(os.environ["CIRCLE_ARTIFACTS"], "coverage-report")
+    cov.html_report(directory=out)
+else:
+    # We're running locally. Use a simpler test runner, and only report
+    # coverage if it's explicitly requested via the --report-coverage line.
+    unittest.TextTestRunner().run(fullsuite)
+    cov.stop()
+    # Report coverage if the flag is enabled
+    if "--report-coverage" in sys.argv:
+        print("\n")
+        cov.report(show_missing=True)
