d24871c479b0fcd7795ee9bf9b62c16595ddfa4c
Pråppe <pw.td.gk@gmail.com>
2016-08-09 23:53:41 -0400

    tests galore for discovery

--- tests/sources.py
+++ tests/sources.py
@@ -4,12 +4,16 @@ media sources
 """
 
 import unittest
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
 # Magically manipulate sys.path
 from testassets import pathmagic
 
+from SocialNPHS.sources.twitter import discover
 from SocialNPHS.sources.twitter import user
 from SocialNPHS.sources.twitter import tweets
+from SocialNPHS.sources.twitter.auth import api
 
 
 # TWITTER TESTS
@@ -47,6 +51,7 @@ class TestUser(unittest.TestCase):
         with self.assertRaises(ValueError):
             user.NPUser("this_is_too_long_to_be_a_twitter_handle")
 
+
 class testTweets(unittest.TestCase):
     """ Test SocialNPHS.sources.twitter.tweets """
     def test_list(self):
@@ -57,11 +62,80 @@ class testTweets(unittest.TestCase):
 
 class testDiscovery(unittest.TestCase):
     """ Test SocialNPHS.sources.twitter.discover """
-    pass
+    # Setting up mock Twitter users to do these tests consistently
+    chris = MagicMock(
+        id='c',
+        location='New Paltz, NY',
+        screen_name='bravoc9',
+        followers_count=2
+    )
+    luke = MagicMock(
+        id='l',
+        location='New Paltz',
+        screen_name='1Defenestrator',
+        followers_count=3
+    )
+    moshe = MagicMock(
+        id='m',
+        location='New Paltz NY',
+        screen_name='G4_Y5_3X',
+        followers_count=2
+    )
+    rock = MagicMock(
+        id='r',
+        location='Da Qiu, China',
+        screen_name='TheRock',
+        followers_count=10390547
+    )
+
+    chris.followers = [luke, moshe]
+    chris.following = [luke, rock]
+
+    luke.followers = [chris, moshe, rock]
+    luke.following = [chris, moshe, rock]
+
+    moshe.followers = [luke, rock]
+    moshe.following = [chris, luke, rock]
+
+    rock.followers = [chris, luke, moshe]
+    rock.following = [luke, moshe]
+
+    def test_association(self):
+        users = discover.discover_by_association(self.luke, 3, 2)
+        # Chris isn't famous and has enough people from NP following him
+        self.assertTrue('bravoc9' in users)
+        # Rock is too famous, Moshe only has 1 NP student following him
+        self.assertFalse('G4_Y5_3X' in users)
+        self.assertFalse('TheRock' in users)
+
+    def test_location(self):
+        users = discover.discover_by_location(self.luke, 6)
+        self.assertTrue('G4_Y5_3X' in users)
+        self.assertFalse('TheRock' in users)
+
+    def test_geolocation(self):
+        mock_tweets = {
+            'c': [MagicMock(place=MagicMock(attributes=MagicMock(
+                postal_code=12561)))],
+            'l': [MagicMock(place=MagicMock(contained_within=MagicMock(
+                name='New Paltz')))],
+            'm': [MagicMock(place=MagicMock(bounding_box=MagicMock(
+                coordinates=[
+                    (-74.005, 41.695), (-74.005, 41.711),
+                    (-73.037, 41.711), (-73.037, 41.695)
+                ])))],
+            'r': [MagicMock(place=None)]
+        }
+        with patch('SocialNPHS.sources.twitter.auth.api') as mock_api:
+            def return_method(id):
+                return mock_tweets[id]
+            mock_api.user_timeline.return_value = return_method
+            users = discover.discover_by_geolocation(self.luke, 6)
+            self.assertTrue('G4_Y5_3X' in users)
+            self.assetFalse('TheRock' in users)
 
 
 # INSTAGRAM TESTS
-
 class TestNotImplemented(unittest.TestCase):
     def test_error(self):
         with self.assertRaises(NotImplementedError):
