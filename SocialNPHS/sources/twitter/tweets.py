04674a8017398abd2d061f280512ee9a3a504f05
Luke Deen Taylor <luke@deentaylor.com>
2016-10-22 16:07:19 -0400

    Support downloading tweets by start time in get_nphs_tweets

--- SocialNPHS/sources/twitter/tweets.py
+++ SocialNPHS/sources/twitter/tweets.py
@@ -2,11 +2,24 @@
 Methods for getting NPHS tweets
 """
 
+from datetime import datetime, timedelta
+
+from tweepy import Cursor
+
 from SocialNPHS.sources.twitter.auth import api
 
 
-def get_nphs_tweets():
+def get_nphs_tweets(since=datetime.utcnow() - timedelta(hours=24)):
     """ Get most recent tweets from the Twitter list of NPHS students """
-    statuses = api.list_timeline("1Defenestrator", "NPHS")
+    statuses = []
+
+    # Find all tweets since the provided datetime
+    for status in Cursor(api.list_timeline, "1Defenestrator", "NPHS").items():
+        if status.created_at < since:
+            break
+        else:
+            statuses.append(status)
+
+    # statuses = api.list_timeline("1Defenestrator", "NPHS")
     # Filter out retweets and return
-    return [s.text for s in statuses if not s.text.startswith("RT @")]
+    return [s for s in statuses if not s.text.startswith("RT @")]
