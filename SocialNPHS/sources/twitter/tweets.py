55b99e3a21b33f422a22be1a9becef0f1e5c3fc9
Luke Deen Taylor <luke@deentaylor.com>
2016-10-22 14:35:05 -0400

    Remove retweets from get_nphs_tweets

--- SocialNPHS/sources/twitter/tweets.py
+++ SocialNPHS/sources/twitter/tweets.py
@@ -8,4 +8,5 @@ from SocialNPHS.sources.twitter.auth import api
 def get_nphs_tweets():
     """ Get most recent tweets from the Twitter list of NPHS students """
     statuses = api.list_timeline("1Defenestrator", "NPHS")
-    return [status.text for status in statuses]
+    # Filter out retweets and return
+    return [s for s in statuses if not s.text.startswith("RT @")]
