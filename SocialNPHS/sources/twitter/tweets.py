7f72fe4bc9aeb4828b35699bd6e5840463b5f9e9
Pråppe <pw.td.gk@gmail.com>
2016-10-22 14:41:38 -0400

    return status text, not the entire status

--- SocialNPHS/sources/twitter/tweets.py
+++ SocialNPHS/sources/twitter/tweets.py
@@ -9,4 +9,4 @@ def get_nphs_tweets():
     """ Get most recent tweets from the Twitter list of NPHS students """
     statuses = api.list_timeline("1Defenestrator", "NPHS")
     # Filter out retweets and return
-    return [s for s in statuses if not s.text.startswith("RT @")]
+    return [s.text for s in statuses if not s.text.startswith("RT @")]
